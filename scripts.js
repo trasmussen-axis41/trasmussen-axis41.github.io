8d099be30efa183169a657ca7827df49e74722a2
Tyler Rasmussen <trasmussen@Trasmussenmac.local>
2017-06-27 09:28:13 -0600

    test

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
 (function() {
 	console.log("Hello");
 	$('.main-nav-links span').on('click',function(){
-		$('.dropdown-container').slideToggle();
+		var element = $(this).data('element');
 		console.log($(this).data('element'));
+		$('.dropdown-container div').hide();
+		$('.dropdown-container div:not[data-element="' + element + '"]').show()
+		$('.dropdown-container').slideToggle();
 	});
 })();
