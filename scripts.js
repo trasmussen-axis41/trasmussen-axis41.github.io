5656c0e1b7b5f5e3b06e9a26ccff0cb094ec7d8f
Tyler Rasmussen <trasmussen@Trasmussenmac.local>
2017-06-28 13:53:58 -0600

    test

--- scripts.js
+++ scripts.js
@@ -8,10 +8,19 @@
 		$('.dropdown-container').slideDown();
 	});
 	$('.main-nav-links span').on('mouseleave',function(evt){
+		var v = document.elementFromPoint(evt.pageX, evt.pageY);
+		var r = $(v);
+		if(r.length) {
+			return;
+		}
 		var element = $(this).data('element');
 		console.log($(this).data('element'));
 		$('.dropdown-container > div').hide();
 		$('.dropdown-container').slideUp();
 	});
+	$('.dropdown-container').on('mouseleave',function(evt)) {
+		$('.dropdown-container > div').hide();
+		$('.dropdown-container').slideUp();
+	}
 
 })();
