d52a28a31c953cfcfe5d825523201c68a3586429
Tyler Rasmussen <trasmussen@Trasmussenmac.local>
2017-06-28 13:44:56 -0600

    test

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@
 		$('.dropdown-container div[data-element="' + element + '"]').show();
 		$('.dropdown-container').slideDown();
 	});
-	$('.main-nav-links span').on('mouseleave',function(){
+	$('.main-nav-links span').on('mouseleave',function(evt){
 		var element = $(this).data('element');
 		console.log($(this).data('element'));
 		$('.dropdown-container > div').hide();
