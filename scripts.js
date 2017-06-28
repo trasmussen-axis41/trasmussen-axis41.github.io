304fc076ceae8ae3f81fe598f19338df6f8770db
Tyler Rasmussen <trasmussen@Trasmussenmac.local>
2017-06-28 13:34:42 -0600

    test

--- scripts.js
+++ scripts.js
@@ -1,10 +1,17 @@
 (function() {
 	console.log("Hello");
-	$('.main-nav-links span').on('mouseenter mouseleave',function(){
+	$('.main-nav-links span').on('mouseenter',function(){
 		var element = $(this).data('element');
 		console.log($(this).data('element'));
 		$('.dropdown-container > div').hide();
-		$('.dropdown-container div[data-element="' + element + '"]').show()
-		$('.dropdown-container').slideToggle();
+		$('.dropdown-container div[data-element="' + element + '"]').show();
+		$('.dropdown-container').slideDown();
 	});
+	$('.main-nav-links span').on('mouseleave',function(){
+		var element = $(this).data('element');
+		console.log($(this).data('element'));
+		$('.dropdown-container > div').hide();
+		$('.dropdown-container').slideUp();
+	});
+
 })();
